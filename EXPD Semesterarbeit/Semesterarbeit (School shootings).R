3080f91c479697768b8a0cc47f18c5891673b2cf
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2023-10-19 13:02:15 +0200

    update 2

--- EXPD Semesterarbeit/Semesterarbeit (School shootings).R
+++ EXPD Semesterarbeit/Semesterarbeit (School shootings).R
@@ -14,7 +14,7 @@ hist(vec_shooter_age, col = "lightblue", xlab = "Age [Years]",
 axis(side=1, at=seq(0, 100, by=10))
 median_age_shooter <- median(vec_shooter_age,na.rm = TRUE)
 abline(v = median_age_shooter, col = "red", lty = 1, lwd = 2)
-legend("topright", legend = paste("Median =", median_age_shooter), col = "red", lty = 1, lwd = 2)
+legend("top", legend = paste("Median =", median_age_shooter), col = "red", lty = 1, lwd = 2)
 
 # Number schoolshootings
 hist(dat$year,
@@ -23,12 +23,14 @@ hist(dat$year,
      xaxt = "n",
      xlab = "Years",
      col = "lightblue")
-axis(side = 1, at = seq(1999, 2023, by = 1))
+axis(side = 1, at = seq(1999, 2023, by = 1), las = 2)
 
 meanshootings <- length(dat$year)/length(unique(dat$year))
 
-abline(h = meanshootings, col = "red", lty = 1, lwd = 2)
-legend("top", legend = paste("Mean Shootings: ", round(meanshootings, 2)), col = "red", lty = 1, lwd = 2)
+# Median
+mediantable <- table(dat$year)
+medianshootings <- median(c(mediantable))
 
-
-
+# Add Median and Legend to plot
+abline(h = medianshootings, col = "red", lty = 1, lwd = 2)
+legend("top", legend = paste("Median Shootings: ", round(medianshootings, 2)), col = "red", lty = 1, lwd = 2)
